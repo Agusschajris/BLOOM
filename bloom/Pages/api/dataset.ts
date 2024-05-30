3880775261ed0f5247ad6746c4dd40bf749a3cba
Aguss <111596201+Agusschajris@users.noreply.github.com>
2024-05-30 15:56:11 -0300

    style: dataset.ts

--- bloom/Pages/api/dataset.ts
+++ bloom/Pages/api/dataset.ts
@@ -12,13 +12,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const { dataset } = req.body;
       const newDataset = await prisma.dataset.create({
         data: {
-          dataset, // Asigna la URL proporcionada
+          dataset,
         },
       });
 
       res.status(201).json(newDataset);
     } else {
-      res.status(405).end(); // Método no permitido
+      res.status(405).end();
     }
   } catch (error) {
     console.error('Error en la API:', error);
