86a5bd32e2773037a7756c45935ee05e3b35ca67
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-01 23:33:28 -0300

    feat(connections): dataset upload
    Co-Authored-By: Aguss <111596201+Agusschajris@users.noreply.github.com>

--- bloom/components/Main.tsx
+++ bloom/components/Main.tsx
@@ -31,9 +31,20 @@ const MainPage: React.FC = () => {
         console.log(response);
       });
     } else {
+      let formData = new FormData();
       Array.from(dataset).forEach(file => {
         console.log('Archivo seleccionado:', file.name);
+        formData.append(file.name, file);
       });
+      formData.append("close", "close");
+      fetch("http://localhost:3000/api/dataset/upload", {
+        method: 'POST',
+        body: formData
+      }).then((response) => {
+        console.log(response);
+      }).catch(err => {
+        console.log(err);
+      })
     }
     setShowPopup(false);
     router.push('/proyecto'); 
