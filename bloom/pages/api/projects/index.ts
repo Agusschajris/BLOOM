c49f2c2f0f6e25f6894dd413732399bb1fab6936
Aguss <111596201+Agusschajris@users.noreply.github.com>
2024-06-11 08:02:23 -0300

    fix: endpoint for projects

--- bloom/pages/api/projects/index.ts
+++ bloom/pages/api/projects/index.ts
@@ -1,4 +1,4 @@
-/*
+
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
@@ -8,29 +8,32 @@ AHora tengo que recibir cuando tengo que crear un nuevo proyecto y prear ese nue
 también poder enviarle al front el proyecto que me esté pidiendo
 https://youtube.com/playlist?list=PLrAw40DbN0l2dg--IB6xTsEQTD1Qb1aBa&si=WRvuy51xJbAG8btb
 */
-/*
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method === 'GET') {
       // Get all projects
-      const userId = 1 //req.query.userId; //--> cuando tenga más de un usuario
+      const userId = 1 // req.query.userId --> para cuando tenga más de un user
       const projects = await prisma.project.findMany({
         where: {
-          userId: userId,
+          id: userId,
         },
       });
       res.status(200).json(projects);
-    }
     } else if (req.method === 'POST') {
-      // Save a new dataset
-      const { dataset } = req.body;
-      const newDataset = await prisma.dataset.create({
+      // Save a new project
+      const { project } = req.body;
+      if (!project || !project.name || !project.ownerId || !project.datasetId) {
+        return res.status(400).json({ error: 'Falta información para crear un proyecto' });
+      }
+      const newProject = await prisma.project.create({
         data: {
-          dataset,
+          name: project.name,
+          ownerId: project.ownerId,
+          datasetId: project.datasetId,
         },
       });
-
-      res.status(201).json(newDataset);
+      res.status(201).json(newProject);
     } else {
       res.status(405).end();
     }
@@ -39,4 +42,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(500).json({ error: 'Error en el servidor' });
   }
 }
-*/
+
