13d8bd8e030504904836b45f7d1f4e11c96f1244
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-30 18:20:07 -0300

    fix(api): projects from ownerId

--- bloom/pages/api/projects/index.ts
+++ bloom/pages/api/projects/index.ts
@@ -1,4 +1,3 @@
-
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
@@ -8,17 +7,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       // Obtener todos los proyectos
       const userId = 1; // req.query.userId --> para cuando tengas más de un usuario
       let orderBy = {};
+      const defaultOrAsc = req.query.orderDirection ? req.query.orderDirection : 'asc';
+      const defaultOrDesc = req.query.orderDirection ? req.query.orderDirection : 'desc';
 
       // Verificar el valor del parámetro orderBy que envía el front
       switch (req.query.orderBy) {
         case 'alfabetic': // Orden alfabético
-          orderBy = { name: req.query.orderDirection === 'asc' ? 'asc' : 'desc' };
+          orderBy = { name: defaultOrAsc };
           break;
         case 'creationDate':
-          orderBy = { creationDate: req.query.orderDirection === 'asc' ? 'asc' : 'desc' };
+          orderBy = { creationDate: defaultOrDesc };
           break;
         case 'lastEdited':
-          orderBy = { lastEdited: req.query.orderDirection === 'asc' ? 'asc' : 'desc' };
+          orderBy = { lastEdited: defaultOrDesc };
           break;
         default: // Por defecto, se ordena por el último editado
           orderBy = { lastEdited: 'desc' };
@@ -26,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       const projects = await prisma.project.findMany({
         where: {
-          id: userId,
+          ownerId: userId,
         },
         orderBy,
       });
