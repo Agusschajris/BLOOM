d24a0932db59777301d003e26c460364209f9ddd
zoeperezc <110468532+zoeperezc@users.noreply.github.com>
2024-06-30 18:03:45 -0300

    fix: import error

--- bloom/components/navBar.tsx
+++ bloom/components/navBar.tsx
@@ -1,44 +1,48 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import './CustomComponent.css';
+import { useRouter } from 'next/router';
 import style from '../styles/navBar.module.scss';
 import Image from 'next/image';
 import logoSVG from '../public/logo.svg';
 import userSVG from '../public/user.svg';
 
-const navBar = () => {
+const NavBar: React.FC = () => {
+  const router = useRouter();
+  const { pathname } = router;
+  
+  let classProyect = "";
+  let classClass = "";
 
-    const handleUser = () => {
-        
-    }
-    const location = useLocation()
-    let classProyect = ""
-    let classClass = ""
+  switch (pathname) {
+    case "/":
+      classProyect = style.selected;
+      classClass = style.notSelected;
+      break;
+    case "/clases":
+      classProyect = style.notSelected;
+      classClass = style.selected;
+      break;
+    default:
+      classProyect = style.notSelected;
+      classClass = style.notSelected;
+      break;
+  }
 
-    switch (location.pathname) {
-        case "/":
-            classProyect = "selected"
-            classClass = "notSelected"
-            break;
-        case "/clases":
-            classProyect = "notSelected"
-            classClass = "selected"
-            break;
-    }
+  const handleUser = () => {
+    
+  }
 
-    return (
-        <div className={style.bar}>
-            <Image src={logoSVG} alt="logo" width={10} height={5} />
-            <div className={style.options}>
-                <button className={style.classProyect}>Proyectos</button>
-                <button className={style.classClass}>Clases</button>
-            </div>
-            <button className={style.userBtn} onClick={handleUser}>
-                <Image src={userSVG} alt="user" width={5} height={5} />
-            </button>
-        </div>
-    )
-
+  return (
+    <div className={style.bar}>
+      <Image src={logoSVG} alt="logo" width={10} height={5} />
+      <div className={style.options}>
+        <button className={classProyect}>Proyectos</button>
+        <button className={classClass}>Clases</button>
+      </div>
+      <button className={style.userBtn} onClick={handleUser}>
+        <Image src={userSVG} alt="user" width={5} height={5} />
+      </button>
+    </div>
+  );
 }
 
-export default navBar;
+export default NavBar;
