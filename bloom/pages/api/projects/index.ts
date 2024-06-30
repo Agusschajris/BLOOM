b5df0fcbad32eb47513d82f3cdb54fa2f5f347e7
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-06-30 17:02:10 -0300

    feat(api): undefined properties + pre-compressed blocks

--- bloom/pages/api/projects/index.ts
+++ bloom/pages/api/projects/index.ts
@@ -47,25 +47,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(201).json(newProject);
     } else if (req.method === 'PUT') {
       const { projectId } = req.query;
-      const { project } = req.body;
-
-      const blocksAsString = project.blocks ? JSON.stringify(project.blocks) : null;
+      let { name, blocks } = req.body;
 
       // Buscar el proyecto por su ID
       const existingProject = await prisma.project.findUnique({
         where: { id: Number(projectId) },
       });
 
-      if (!existingProject) {
+      if (!existingProject)
         return res.status(404).json({ error: 'Proyecto no encontrado' });
-      }
+
+      if (!name || typeof name !== 'string') name = existingProject.name;
+      if (!blocks || typeof blocks !== 'string') blocks = existingProject.blocks;
 
       // Actualizo
       await prisma.project.update({
         where: { id: Number(projectId) },
         data: {
-          name: project.name,
-          blocks: blocksAsString,
+          name,
+          blocks,
           lastEdited: { set: new Date() },
         },
       });    
