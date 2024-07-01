bbf806e090ae7934373a1ea879503f8628542c6b
zoeperezc <110468532+zoeperezc@users.noreply.github.com>
2024-07-01 17:26:32 -0300

    style: proyectPrev

--- bloom/components/proyectPrev.tsx
+++ bloom/components/proyectPrev.tsx
@@ -54,8 +54,7 @@ const ProyectPrev: React.FC<ProyectPrevProps> = ({id, name}) => {
 
     return (
         <div className={style.container}>
-            <div className={style.preview}>
-            </div>
+            <div className={style.preview}></div>
             <div className={style.footer}>
                 <p className={style.name}>{name}</p>
                 <button className={style.edit} onClick={handleEdit}>Editar</button>
