edb39505db6ea00b2d9b0ee5456f41f9a42c724b
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-09-23 10:44:43 -0300

    feat(arguments): better input types

--- bloom/components/arguments.tsx
+++ bloom/components/arguments.tsx
@@ -6,35 +6,35 @@ import ticSVG from "../public/tic.svg"
 import Image from 'next/image';
 
 interface PopupProps {
-    block: BlockInstance;
-    onClose: () => void;
-    onSave: (args: Argument[]) => void;
+  block: BlockInstance;
+  onClose: () => void;
+  onSave: (args: Argument[]) => void;
 }
 
 const Popup: React.FC<PopupProps> = ({ block, onClose, onSave }) => {
-    const [args, setArgs] = useState<Argument[]>(block.args);
+  const [args, setArgs] = useState<Argument[]>(block.args);
 
-    const handleChange = (index: number, value: any) => {
-        const newArgs = [...args];
-        newArgs[index].value = value;
-        setArgs(newArgs);
-    };
+  const handleChange = (index: number, value: any) => {
+    const newArgs = [...args];
+    newArgs[index].value = value;
+    setArgs(newArgs);
+  };
 
-    const handleSave = () => {
-        const isValid = args.every(arg => {
-            if (arg.type === 'number') {
-                return !isNaN(Number(arg.value));
-            }
-                 true;
-            });
+  const handleSave = () => {
+    const isValid = args.every(arg => {
+      if (arg.type === 'number') {
+        return !isNaN(Number(arg.value));
+      }
+      return true;
+    });
         
-            if (isValid) {
-                onSave(args);
-                onClose();
-            } else {
-                alert('Invalid values');
-        }
-    };
+    if (isValid) {
+      onSave(args);
+      onClose();
+    } else {
+      alert('Invalid values');
+    }
+  };
 
   return (
     <div>
@@ -43,18 +43,34 @@ const Popup: React.FC<PopupProps> = ({ block, onClose, onSave }) => {
         <div className={style.header}>
           <h1>{block.visualName}</h1>
           <button className={style.cancel} onClick={onClose}>
-                    <Image src={closeSVG} alt="close" width={15} height={15} />
+            <Image src={closeSVG} alt="close" width={15} height={15} />
           </button>
         </div>
         {args.map((arg, index) => (
           <div className={style.argWrapper} key={arg.argName}>
             <label className={style.argName} >{arg.visualName}</label>
-            <input
-              placeholder='---'
-              className={style.input}
-              type={arg.type === 'number' ? 'number' : 'text'}
-              onChange={(e) => handleChange(index, e.target.value)}
-            />
+            {
+              arg.type === 'select' ? (
+                <select
+                  className={style.input}
+                  required={null !in (arg.values as (string | null)[])}
+                  onChange={(e) => handleChange(index, e.target.value)}
+                  value={(arg.default as string|null) ?? ''}
+                >
+                  {(arg.values! as (string | null)[]).map((value, i) => (
+                    <option value={value ?? ''} key={i}>{value ?? 'null'}</option>
+                  ))}
+                </select>
+              ) : (
+                <input
+                  placeholder='---'
+                  className={style.input}
+                  type={arg.type}
+                  onChange={(e) => handleChange(index, e.target.value)}
+                  value={arg.type !== 'checkbox' ? arg.default as undefined|null|string|number ?? '' : ''}
+                  checked={arg.default as boolean|null ?? false}
+                />)
+            }
           </div>
         ))}
         <button className={style.save} onClick={handleSave}>
